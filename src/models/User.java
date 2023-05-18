a257e1ec537263d2959a86712e8ce957ade860ed
daiki.okabe <daiki-okabe@xuis.co.jp>
2023-05-18 14:03:24 +0900

    DB関連ファイル

---
+++ src/models/User.java
@@ -0,0 +1,115 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "M_USER")
+public class User {
+    @Id
+    @Column(name = "USER_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_id;
+
+    @Column(name = "USER_NAME", length = 200, nullable = false)
+    private String user_name;
+
+    @Column(name = "DEPT_ID", nullable = false)
+    private Integer dept_id;
+
+    @Column(name = "USER_ROLE", nullable = false)
+    private Integer user_role;
+
+    @Column(name = "USER_CLASS", nullable = false)
+    private Integer user_class;
+
+    @Column(name = "INP_USER", length = 200)
+    private String inp_user;
+
+    @Column(name = "INP_DATE")
+    private Timestamp inp_date;
+
+    @Column(name = "UPD_USER", length = 200)
+    private String upd_user;
+
+    @Column(name = "UPD_DATE")
+    private Timestamp upd_date;
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public Integer getDept_id() {
+        return dept_id;
+    }
+
+    public void setDept_id(Integer dept_id) {
+        this.dept_id = dept_id;
+    }
+
+    public Integer getUser_role() {
+        return user_role;
+    }
+
+    public void setUser_role(Integer user_role) {
+        this.user_role = user_role;
+    }
+
+    public Integer getUser_class() {
+        return user_class;
+    }
+
+    public void setUser_class(Integer user_class) {
+        this.user_class = user_class;
+    }
+
+    public String getInp_user() {
+        return inp_user;
+    }
+
+    public void setInp_user(String inp_user) {
+        this.inp_user = inp_user;
+    }
+
+    public Timestamp getInp_date() {
+        return inp_date;
+    }
+
+    public void setInp_date(Timestamp inp_date) {
+        this.inp_date = inp_date;
+    }
+
+    public String getUpd_user() {
+        return upd_user;
+    }
+
+    public void setUpd_user(String upd_user) {
+        this.upd_user = upd_user;
+    }
+
+    public Timestamp getUpd_date() {
+        return upd_date;
+    }
+
+    public void setUpd_date(Timestamp upd_date) {
+        this.upd_date = upd_date;
+    }
+}
